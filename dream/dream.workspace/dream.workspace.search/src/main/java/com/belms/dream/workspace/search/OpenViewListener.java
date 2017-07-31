b3ac02d09691ed304ed045032a12eea59c87fd53
phanny <phanny@DESKTOP-81J878I>
2017-08-01 02:05:41 +0700

    clean

--- dream/dream.workspace/dream.workspace.search/src/main/java/com/belms/dream/workspace/search/OpenViewListener.java
+++
@@ -1,7 +0,0 @@
-package com.belms.dream.workspace.search;
-
-import com.blems.dream.api.model.ui.FilterItemList;
-
-public interface OpenViewListener {
-	void open(FilterItemList filterItemList);
-}
